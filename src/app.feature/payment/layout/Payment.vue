494e58052d76f5798c4260267f740c1342ef1da0
csycp <csik@acalab.co.kr>
2023-02-01 18:06:28 +0900

    style: addheader

--- src/app.feature/payment/layout/Payment.vue
+++ src/app.feature/payment/layout/Payment.vue
@@ -1,9 +1,21 @@
-<template><Header /></template>
+<template>
+<Header />
+<div class="whole-page">
+
+</div>
+</template>
 <script>
 import Header from "~/app.component/Header.vue";
+import TextView from "~/app.component/TextView.vue";
 
 export default {
   components: { Header },
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.whole-page {
+  width: 100%;
+  height: 740px;
+  background: linear-gradient(180deg, #FFFFFF 0%, #FBF8FF 33.33%, #F0E4FF 77.6%, #E1ECFF 100%);
+}
+</style>
