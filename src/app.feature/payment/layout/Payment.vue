dd298e7a5c41b9e1b909fb30cb786c885095254c
csycp <csik@acalab.co.kr>
2023-02-01 16:38:06 +0900

    style: set payment files

---
+++ src/app.feature/payment/layout/Payment.vue
@@ -0,0 +1,9 @@
+<template><Header /></template>
+<script>
+import Header from "~/app.component/Header.vue";
+
+export default {
+  components: { Header },
+};
+</script>
+<style lang="scss"></style>
---
+++ src/views/Payment1.vue
@@ -0,0 +1,6 @@
+<template>
+  <Payment />
+</template>
+<script setup>
+import Payment from "~/app.feature/payment/layout/Payment.vue";
+</script>
